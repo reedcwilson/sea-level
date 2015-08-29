7ad8e3747fd6e503dc632868ebc323bb34ce2cff
Reed Wilson <reedcwilson@gmail.com>
2015-08-28 23:18:07 -0600

    Initial Commit

---
+++ sea-level/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sea-level
+//
+//  Created by Reed Wilson on 8/28/15.
+//  Copyright (c) 2015 Reed Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
